2e7807f76acc44d0888a553d8679d867d6313781
Minh-Quan TRAN <mqtran@ijenko.com>
2014-04-24 19:07:38 +0200

    update

--- GlobalInstaller/Composer/GlobalInstallerPlugin.php
+++ GlobalInstaller/Composer/GlobalInstallerPlugin.php
@@ -6,7 +6,7 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-class TemplateInstallerPlugin implements PluginInterface
+class GlobalInstallerPlugin implements PluginInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
